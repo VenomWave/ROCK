50750345e94cbd8247ace2d416ad85e08e0ff714
AADITHYA KRISHNA RAJ <aadithyakredits@gmail.com>
2025-08-21 08:19:52 -0700

    Update db_config.py
    Signed-off-by: AADITHYA KRISHNA RAJ <aadithyakredits@gmail.com>

--- db_config.py
+++ db_config.py
@@ -1,9 +1,9 @@
 # config_db.py
 
-import sqlite3
+import mysql.connector
 
 def create_database():
-    conn = sqlite3.connect('rock.db')
+    conn = mysql.connector.connect('rock.db')
     cursor = conn.cursor()
     cursor.execute('''CREATE TABLE complaints(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT NOT NULL,email TEXT NOT NULL,complaint TEXT NOT NULL,status TEXT DEFAULT 'Pending')''')
     conn.commit()
